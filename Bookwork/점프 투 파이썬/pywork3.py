0397b34b57b68c8f2b796a57f342697a99ebc0f8
skwang <skwang89@gmail.com>
2020-08-11 21:48:55 +0900

    add pywork4

--- Bookwork/점프 투 파이썬/pywork3.py
+++ Bookwork/점프 투 파이썬/pywork3.py
@@ -237,35 +237,188 @@ f.close()
 # close() 함수를 이용해서 직접 닫아 주는 것이 좋다.
 
 # 파일을 쓰기 모드로 열어 출력값 적기
+f = open("C:\workspace-total\pythonworkspace\점프 투 파이썬\Bookwork\점프 투 파이썬/새파일.txt", 'w')
+for i in range(1, 11):
+    data = "%d번째 줄입니다.\n" % i
+    f.write(data)
+f.close()
+
+# 프로그램의 외부에 저장된 파일을 읽는 여러 가지 방법
+# readline() 함수 이용하기
+f = open("C:/doit/새파일.txt", 'r')
+line = f.readline()
+print(line)
+f.close()
+# 파일.txt의 가장 첫 번째 줄이 화면에 출력됨
+
+f = open("C:/doit/새파일.txt", 'r')
+while True:
+    line = f.readline()
+    if not line: break
+    print(line)
+f.close()
+# 파일.txt의 모든 줄이 화면에 출력됨
+# 사용자의 입력을 받아서 그 내용을 출력하는 경우
+while 1:
+    data = input()
+    if not data: break
+    print(data)
 
 
+# readlines 함수 사용하기
+f = open("C:/새파일.txt", 'r')
+lines = f.readlines()
+for line in lines:
+    print(line)
+f.close()
+# 모든 줄을 읽어서 각각의 줄을 요소로 갖는 리스트로 돌려준다.
+# f.readlines()에서 f.readline()과 차이 유의
 
+# read 함수 사용하기
+f = open("C:/새파일.txt", 'r')
+data = f.read()
+print(data)
+f.close()
+# 파일의 내용 전체를 문자열로 돌려준다.
 
+# 파일에 새로운 내용 추가하기: 파일 추가 모드('a')
+f = open("C:/새파일.txt",'a')
+for i in range(11, 20):
+    data = "%d번째 줄입니다.\n" % i
+    f.write(data)
+f.close()
+# 새파일.txt 파일을 추가 모드('a')로 열고
+# write를 사용해서 결괏값을 기존 파일에 추가해 적는 예
 
+# with문과 함께 사용하기: 파일을 열고 닫는 것을 자동으로 처리
+f = open("foo.txt", 'w')
+f.write("Life is too short, you need python")
+f.close()
 
+with open("foo.txt", "w") as f:
+    f.write("Life is too short, you need python")
+# with 블록을 벗어나는 순간 열린 파일 객체 f가 자동으로 close된다
+# sys 모듈로 매개변수 주기
+# 명령 프롬프트 명령어 [인수1 인수2 ...]
+# 파이썬에서는 sys 모듈을 사용하여 매개변수를 직접 줄 수있다.
+# sys 모듈을 사용하려면 아래 예의 import sys처럼 import 명령어를 사용해야 한다.
+import sys
 
+args = sys.argv[1:]
+for i in args:
+    print(i)
 
+# C:\>python sys1.py aaa bbb ccc
+# aaa
+# bbb
+# ccc
 
+import sys
+args = sys.argv[1:]
+for i in args:
+    print(i.upper(), end=' ')
+# 문자열 관련 함수인 upper()를 사용하여 명령 행에 입력된 소문자를
+# 대문자로 바꾸어 주는 간단한 프로그램이다.
+# 명령프롬프트 창에서 다음과 같이 입력해 보자.
+#
+# ※ sys2.py 파일이 C:\doit 디렉터리 안에 있어야만 한다.
 
+# LIFE IS TOO SHORT, YOU NEED PYTHON
 
+# <연습문제>
+# Q1
+# 주어진 자연수가 홀수인지 짝수인지 판별해 주는 함수(is_odd)를 작성해 보자.
+def odd_even(a):
+    if a%2==0:
+        return '짝수'
+    else:
+        return '홀수'
+odd_even(4)
+# 짝수
+odd_even(11)
+# 홀수
 
+#람다와 조건부 표현식 사용한 해답
+odd_even = lambda a: "짝수" if a%2==0 else "홀수"
+odd_even(14)
+# 짝수
 
+# Q2
+# 입력으로 들어오는 모든 수의 평균 값을 계산해 주는 함수를 작성해 보자.
+# (단 입력으로 들어오는 수의 개수는 정해져 있지 않다.)
+# ※ 평균 값을 구할 때 len 함수를 사용해 보자.
+def avg(*args):
+    result = 0
+    for i in args:
+        result += i
+    return result/len(args)
+avg(1,2)            # 1.5
+avg(1,2,3,4,5)      # 3.0
 
+# Q3
+# 다음은 두 개의 숫자를 입력받아 더하여 돌려주는 프로그램이다.
+input1 = input("첫번째 숫자를 입력하세요:")
+input2 = input("두번째 숫자를 입력하세요:")
+total = input1 + input2
+print("두 수의 합은 %s 입니다" % total)
+# 이 프로그램을 수행해 보자.
+# 첫번째 숫자를 입력하세요:3
+# 두번째 숫자를 입력하세요:6
+# 두 수의 합은 36 입니다
+# 3과 6을 입력했을 때 9가 아닌 36이라는 결괏값을 돌려주었다. 이 프로그램의
+# 오류를 수정해 보자.
+# ※ int 함수를 사용해 보자.
+total = int(input1) + int(input2)
+# 입력은 항상 문자열 이므로 숫자로 바꾸어 주어야 한다.
 
+# Q4
+# 다음 중 출력 결과가 다른 것 한 개를 골라 보자.
+print("you" "need" "python")
+print("you"+"need"+"python")
+print("you", "need", "python")
+print("".join(["you", "need", "python"]))
+# 1,2,4: youneedpython
+# 3: you need python 콤마가 있는경우 공백이 삽입되어 더해진다
 
+# Q5
+# 다음은 "test.txt"라는 파일에 "Life is too short"
+# 문자열을 저장한 후 다시 그 파일을 읽어서 출력하는 프로그램이다.
+f1 = open("test.txt", 'w')
+f1.write("Life is too short")
+# f1.close()
+f2 = open("test.txt", 'r')
+print(f2.read())
+# f2.close()
+# 이 프로그램은 우리가 예상한 "Life is too short"라는 문장을 출력하지 않는다.
+# 우리가 예상한 값을 출력할 수 있도록 프로그램을 수정해 보자.
 
+# 일을 닫지 않은 상태에서 다시 열면 파일에 저장한 데이터를 읽을 수 없다.
+# 따라서 열린 파일 객체를 close로 닫아준 후 다시 열어서 파일의 내용을 읽어야 한다.
 
+# Q6
+# 사용자의 입력을 파일(test.txt)에 저장하는 프로그램을 작성해 보자.
+# (단 프로그램을 다시 실행하더라도 기존에 작성한 내용을 유지하고
+# 새로 입력한 내용을 추가해야 한다.)
+user = input('입력')
+f1 = open("test.txt", 'a')
+f1 = write(user)
+# f1 = write("\n")
+f1.close()
 
-
-
-
-
-
-
-
-
-
-
+# Q7
+# 다음과 같은 내용을 지닌 파일 test.txt가 있다. 이 파일의 내용 중
+# "java"라는 문자열을 "python"으로 바꾸어서 저장해 보자.
+#
+# Life is too short
+# you need java
+# ※ replace 함수를 사용해 보자.
+f = open('text.txt', 'r')
+get = f.read()
+f.close()
+get = get.replace('java', 'python')
+f = open('test.txt', 'w')
+f.write(get)
+f.close()
 
 
 # <<교재 4장 끝>>
