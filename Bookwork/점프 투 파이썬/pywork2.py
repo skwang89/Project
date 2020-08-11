55902a23dc736ca2c7fe80c4fa1feba5c395a6ef
skwang <skwang89@gmail.com>
2020-08-11 20:09:20 +0900

    add pywork3

--- Bookwork/점프 투 파이썬/pywork2.py
+++ Bookwork/점프 투 파이썬/pywork2.py
@@ -196,36 +196,189 @@ test_list = ['one', 'two', 'three']
 for i in test_list:
     print(i)
 ...
-one
-two
-three
+# one
+# two
+# three
+
+# 2. 다양한 for문의 사용
+a = [(1,2), (3,4), (5,6)]
+for (first, last) in a:
+    print(first + last)
+...
+# 3
+# 7
+# 11
+# a 리스트의 요솟값이 튜플이기 때문에
+# 각각의 요소가 자동으로 (first, last) 변수에 대입된다.
+
+# 3. for문의 응용
+# "총 5명의 학생이 시험을 보았는데 시험 점수가 60점이 넘으면 합격이고 그렇지 않으면 불합격이다. " \
+# "합격인지 불합격인지 결과를 보여 주시오."
+marks = [90, 25, 67,45, 80]
+number = 0
+for mark in marks:
+    number = number + 1
+    if mark >=60:
+        print('%d번 학생은 합격' %number)
+    else:
+        print('%d번 학생은 불합격' %number)
+
+# for문과 continue
+# while문에서 살펴본 continue문을 for문에서도 사용할 수 있다.
+# 즉 for문 안의 문장을 수행하는 도중에 continue문을 만나면
+# for문의 처음으로 돌아가게 된다.
+for mark in marks:
+    number = number + 1
+    if mark >=60:
+        print('%d번 학생은 합격' %number)
+    else:
+        continue
+
+for mark in marks:
+    number = number +1
+    if mark < 60:
+        continue
+    print("%d번 학생 축하합니다. 합격입니다. " % number)
+
+# for문과 함께 자주 사용하는 range 함수
+a = range(10)
+a           # range(0, 10)
+a = range(1, 11)
+a
+# range 함수의 예시 살펴보기
+add = 0
+for i in range(1, 11):
+    add = add + i
+print(add)
+
+number = 0
+marks = [90, 25, 67,45, 80]
+for number in range(len(marks)):
+    if marks[number] < 60:
+        continue
+    print("%d번 학생 합격" %(number+1))
+
+# for와 range를 이용한 구구단 결과 출력
+for i in range(2,10):
+    for j in range(1,10):
+        print(i*j, end=" ")
+    print()
+# 2 4 6 8 10 12 14 16 18
+# 3 6 9 12 15 18 21 24 27
+# 4 8 12 16 20 24 28 32 36
+# 5 10 15 20 25 30 35 40 45
+# 6 12 18 24 30 36 42 48 54
+# 7 14 21 28 35 42 49 56 63
+# 8 16 24 32 40 48 56 64 72
+# 9 18 27 36 45 54 63 72 81
+# 매개변수 end: 다음줄로 넘기지 않고 그 줄에 계속해서 출력
+
+# 리스트 내포 사용하기
+# 사용전
+a = [1,2,3,4]
+result = []
+for num in a:
+    result.append(num*3)
+print(result)
+# [3, 6, 9, 12]
+
+# 사용후
+a = [1,2,3,4]
+result = [num*3 for num in a]
+print(result)
+# [3, 6, 9, 12]
+
+# 리스트 내포: if 조건 사용
+a = [1,2,3,4]
+result = [num*3 for num in a if num%2==0]
+print(result)
+# [6 ,12]
+
+# 리스트 내포 일반문법
+# [표현식 for 항목 in 반복가능객체 if 조건문]
+# for문을 2개 이상 사용한 리스트 내포 문법
+# [표현식 for 항목1 in 반복가능객체1 if 조건문1
+#             for 항목2 in 반복가능객체2 if 조건문2
+#             ...
+#             for 항목n in 반복가능객체n if 조건문n]
+
+# 리스트 내포를 사용한 구구단 결과
+result = [x*y for x in range(2,10)
+              for y in range(1,10)]
+print(result)
+# [2, 4, 6, 8, 10, 12, 14, 16, 18,
+#  3, 6, 9, 12, 15, 18, 21, 24, 27,
+#  4, 8, 12, 16, 20, 24, 28, 32, 36,
+#  5, 10, 15, 20, 25, 30, 35, 40, 45,
+#  6, 12, 18, 24, 30, 36, 42, 48, 54,
+#  7, 14, 21, 28, 35, 42, 49, 56, 63,
+#  8, 16, 24, 32, 40, 48, 56, 64, 72,
+ # 9, 18, 27, 36, 45, 54, 63, 72, 81]
+
+# <연습문제>
+# Q1
+# 다음 코드의 결괏값은 무엇일까?
+
+a = "Life is too short, you need python"
+if "wife" in a: print("wife")
+elif "python" in a and "you" not in a: print("python")
+elif "shirt" not in a: print("shirt")
+elif "need" in a: print("need")
+else: print("none")
+
+# 가장 먼저 참이된 세번째 조건문 shirt가 출력된다.
+
+# Q2
+# while문을 사용해 1부터 1000까지의 자연수 중 3의 배수의 합을 구해 보자.
+num = 0
+result = 0
+while num < 1000:
+    num = num + 1
+    if num%3==0:
+        result = result + num
+
+print(result)
+
+# Q3
+# while문을 사용하여 다음과 같이 별(*)을 표시하는 프로그램을 작성해 보자.
+#
+# *
+# **
+# ***
+# ****
+# *****
+i = 0
+while i < 5:
+    i += 1
+    print('*'*i)
+
+# Q4
+# for문을 사용해 1부터 100까지의 숫자를 출력해 보자.
+i = 0
+for i in range(1, 101):
+    print(i, end=" ")
+
+# Q5
+# A 학급에 총 10명의 학생이 있다. 이 학생들의 중간고사 점수는 다음과 같다.
+# [70, 60, 55, 75, 95, 90, 80, 80, 85, 100]
+# for문을 사용하여 A 학급의 평균 점수를 구해 보자.
+A = [70, 60, 55, 75, 95, 90, 80, 80, 85, 100]
+t = 0
+for i in A:
+    t += i
+print(t/len(A))
+
+# Q6
+# 리스트 중에서 홀수에만 2를 곱하여 저장하는 다음 코드가 있다.
+numbers = [1, 2, 3, 4, 5]
+result = []
+# for n in numbers:
+#     if n % 2 == 1:
+#         result.append(n*2)
+# 위 코드를 리스트 내포(list comprehension)를 사용하여 표현해 보자.
+result = [n*2 for n in numbers if n%2==1]
+print(result)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# <<교재 3장 시작>>
-# pywork2.py start
-
+# <<교재 3장 끝>>
+# pywork2.py end
