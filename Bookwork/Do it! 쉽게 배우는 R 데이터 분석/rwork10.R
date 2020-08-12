474f7f46b73bc7cb3ab3e15eafc892843d64eb0e
skwang <skwang89@gmail.com>
2020-08-12 22:48:14 +0900

    add rwork12 10 ~ 12장 다시해야됨 (버전다운)

--- Bookwork/Do it! 쉽게 배우는 R 데이터 분석/rwork10.R
+++ Bookwork/Do it! 쉽게 배우는 R 데이터 분석/rwork10.R
@@ -3,3 +3,103 @@
 
 
 # 단계 구분도(Choropleth Map)
+# ● 지역별 통계치를 색깔의 차이로 표현한 지도
+# ● 인구나 소득 같은 특성이 지역별로 얼마나 다른지 쉽게 이해할 수 있음
+
+# 패키지 설치
+install.packages("ggiraphExtra")
+library(ggiraphExtra)
+
+# 데이터 확인하기
+str(USArrests)
+
+# 상위 6개 데이터 확인
+head(USArrests)
+#              Murder Assault UrbanPop Rape
+# Alabama      13.2     236       58 21.2
+# Alaska       10.0     263       48 44.5
+# Arizona       8.1     294       80 31.0
+# Arkansas      8.8     190       50 19.5
+# California    9.0     276       91 40.6
+# Colorado      7.9     204       78 38.7
+
+# 주(state)에 대한 변수 설정 : state
+library(tibble)
+
+crime <- rownames_to_column(USArrests, var = "stste")
+head(crime)
+#      state   Murder Assault UrbanPop Rape
+# 1    Alabama   13.2     236       58 21.2
+# 2     Alaska   10.0     263       48 44.5
+# 3    Arizona    8.1     294       80 31.0
+# 4   Arkansas    8.8     190       50 19.5
+# 5 California    9.0     276       91 40.6
+# 6   Colorado    7.9     204       78 38.7
+
+# 주(state) 데이터를 소문자로 변경
+crime$stste <- tolower(crime$stste)
+head(crime)
+#        state Murder Assault UrbanPop Rape
+# 1    alabama   13.2     236       58 21.2
+# 2     alaska   10.0     263       48 44.5
+# 3    arizona    8.1     294       80 31.0
+# 4   arkansas    8.8     190       50 19.5
+# 5 california    9.0     276       91 40.6
+# 6   colorado    7.9     204       78 38.7
+
+crime <- rename(crime, 'state'='stste')
+head(crime)
+
+
+# map출력을 위한 패키지 설치
+install.packages("maps")
+library(maps)
+
+# 미국 주(state)지도 데이터 불러오기
+library(ggplot2)
+
+state_map <- map_data("state")
+str(state_map)
+head(state_map)
+state_map
+
+# 단계 구분도
+install.packages("mapproj")
+library(mapproj)
+
+ggChoropleth(data = crime,          # 지도에 표시할 데이터
+             aes(fill = Murder,     # 색깔로 표현할 변수
+                 map_id = state),   # 지역 기준 변수
+             map = states_map)      # 지도 데이터
+
+# 인터렉티브 단계 구분도
+ggChoropleth(data = crime,
+             aes(fill = Murder,
+                 map_id = state),
+             map = state_map,
+             interactive = T)
+
+#-------------------------------------------------------------
+# 대한민국 시도별 인구 단계 구분도
+
+# 패키지 설치하기
+install.packages("staringi")
+
+install.packages("devtools")
+library(kormaps2014)
+devtools::install_github("cardiomoon/kormaps2014")
+
+
+
+> install.packages("staringi")
+Warning in install.packages :
+  package ‘staringi’ is not available (for R version 4.0.2)
+
+
+> install.packages("kormaps2014")
+Warning in install.packages :
+  package ‘kormaps2014’ is not available (for R version 4.0.2)
+
+
+# <<교재 11장 끝>>
+# rwork10.R end
