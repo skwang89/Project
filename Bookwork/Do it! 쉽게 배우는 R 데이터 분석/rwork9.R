02c09a1b2271331ebcd58bc9351168e391ff70fd
skwang <skwang89@gmail.com>
2020-08-13 15:31:24 +0900

    add

--- Bookwork/Do it! 쉽게 배우는 R 데이터 분석/rwork9.R
+++ Bookwork/Do it! 쉽게 배우는 R 데이터 분석/rwork9.R
@@ -117,10 +117,85 @@ wordcloud(words = df_word$word,    # 단어
 #-------------------------------------------------------------
 # 국정원 트윗 텍스트 마이닝
 
+# 데이터 로딩
+twitter <- read.csv("data/twitter.csv",      
+                    header = T,              # 헤드 사용
+                    stringsAsFactors = F,    # 문자로 읽어옴 
+                    fileEncoding = "utf-8")  # utf-8로 읽어옴 
+twitter
 
+# 변수명 수정 
+twitter <- rename(twitter,
+                  no = 번호,
+                  id = 계정이름,
+                  date = 작성일,
+                  tw = 내용 )
+twitter
+
+
+# 특수 문자 제거
+library(stringr)
+
+twitter$tw <- str_replace_all(twitter$tw, "\\W", " ")
+head(twitter$tw)
+
+# 명사 추출
+nouns <- extractNoun(twitter$tw)
+nouns
+
+# 추출한 명사 list를 문자열 벡터로 변환, 단어별 빈도 구하기
+wordcount <- table(unlist(nouns))
+wordcount
+
+# 데이터 프레임으로 변환
+df_word <- as.data.frame(wordcount, stringsAsFactors = F)
+df_word
+
+# 변수명 수정 : Var1 -> word, Freq -> freq
+df_word <- rename(df_word,
+                  word = Var1,
+                  freq = Freq)
+head(df_word)
+
+# 2글자 이상의 단어 추출
+df_word <- filter(df_word, nchar(word) >= 2)
+df_word
+
+# 빈도수가 높은 상위 20개 단어 추출
+top20 <- df_word %>% 
+  arrange(desc(freq)) %>%  #빈도수를 기준으로 내림차순 정렬 
+  head(20)
+top20
+
+# top20 단어를 빈도 막대 그래프로 출력
+library(ggplot2)
+
+ggplot(data = top20, aes(x=word, y=freq)) + geom_col()
+
+
+# 워드 클라우드 만들기
+# 패키지 로딩 
+library(wordcloud)
+library(RColorBrewer)
+
+# 색상표
+pal <- brewer.pal(8, "Dark2")
+# pal <- brewer.pal(8, "Blues")
+
+# 난수 고정
+set.seed(1234)
+
+# 워드클라우드
+wordcloud(words = df_word$word,
+          freq = df_word$freq,
+          min.freq = 1,
+          max.words = 300,
+          random.order = F,
+          rot.per = .1,
+          scale = c(6, 0.1),
+          colors = pal )
 
 
 
 # <<교재 10장 끝>>
 # rwork9.R end
-
