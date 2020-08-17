34961ef32c00c88e07a0cad242b2252a372a02b5
skwang <skwang89@gmail.com>
2020-08-17 15:31:17 +0900

    add pywork16

--- Bookwork/점프 투 파이썬/pywork16.py
+++ Bookwork/점프 투 파이썬/pywork16.py
@@ -3,7 +3,28 @@
 
 
 # wordcloud
+import numpy as np
+import matplotlib.pyplot as plt
+import platform
+from wordclous import wordCloud, STOPWORDS
+from PIL import Image
 
+# data1/alice.txt  파일 읽어오기
+text = open('data1/alice.txt', 'r').read()
+print(text)
+
+# data1/alice_mast.png 이미지 읽어오기
+alice_mask = np.array(Image.open('data1.alice_mask.png'))
+# print(alice_mask)
+
+# 제오할 단어를 등록
+stopwords = set(STOPWORDS)
+stopwords.add('said')
+
+# wordcloud 생성
+wc = WordCloud(
+                backg
+)
 
 
 
