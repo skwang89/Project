f2347c670a32116a51c327759907460fda568003
skwang <skwang89@gmail.com>
2020-08-17 20:10:29 +0900

    add pywork18

--- Bookwork/점프 투 파이썬/pywork17.py
+++ Bookwork/점프 투 파이썬/pywork17.py
@@ -945,16 +945,5 @@ plt.savefig('pairplot.png')
 plt.show()
 
 
-
-all = [var for var in globals() if var[0] != "-"]
-for var in all:
-    del globals()[var]
-del(all)
-del(var)
-
-import sys
-sys.modules[__name__].__dict__.clear()
-
-
 # <<강의 복습 10. 끝>>
 # pywork17.py end
